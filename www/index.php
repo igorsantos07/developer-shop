093144711ca7a54184be41fa783659ddfd4c4c5e
Igor Santos <igorsantos07@gmail.com>
2015-12-16 18:59:49 -0200

    Fixing stupid link to bootstrap CSS on CDN

--- www/index.php
+++ www/index.php
@@ -13,7 +13,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <? if (PROD): ?>
-        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"></script>
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <? else: ?>
         <link rel="stylesheet" href="/lib/bootstrap/bootstrap.css">
     <? endif ?>
