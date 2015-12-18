801dbd21c808d3e9cbe984a86d1cac88eb921c10
Igor Santos <igorsantos07@gmail.com>
2015-12-18 00:22:09 -0200

    Fixing sum round issue with prices using JS internal functions

--- src/assets/scripts/utils.js
+++ src/assets/scripts/utils.js
@@ -1,23 +1,11 @@
 module.exports = {
-    priceFormat: function(price) {
+    priceFormat: function(price, showZero) {
         price = parseFloat(price);
         if (typeof price == 'number') {
             if (!price) {
-                price = 'free';
+                price = showZero? '$ 0' : 'free';
             } else {
-                price = price.toString();
-                if (price.indexOf('.') == -1) {
-                    price += '.00';
-                } else {
-                    var parts = price.split('.');
-                    var decimals = parts[1].substr(0, 2);
-                    if (decimals.length < 2) {
-                        decimals += '0';
-                    }
-                    price = parts[0] + '.' + decimals;
-                }
-
-                price = '$ ' + price;
+                price = '$ ' + price.toFixed(2);
             }
         }
         return price;
