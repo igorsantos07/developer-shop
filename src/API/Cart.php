f4bcd5b2feff0de35167380e94729d1db5406331
Igor Santos <igorsantos07@gmail.com>
2015-12-21 02:13:45 -0200

    Updating Restler so we get the Explorer for free, and implementing inclusion of item in cart in the backend. refs #1

--- src/API/Cart.php
+++ src/API/Cart.php
@@ -5,8 +5,26 @@ use Shop\Model\Order;
 
 class Cart {
 
-    public function get() {
+    /**
+     * Gets all items in the cart for the given user session.
+     * @param int $id
+     * @throws 501
+     */
+    public function get($id) {
+        throw new RestException(HTTP_NOT_IMPLEMENTED);
+    }
 
+    /**
+     * Adds an item to the cart.
+     * @status 201
+     * @param string $item The item name {@from body}
+     * @param float $price The item price {@from body}
+     * @return Item
+     */
+    public function put($item, $price) {
+        $order = Order::firstOrCreate(['session' => session_id()]);
+        $item  = $order->items()->create(compact('item', 'price'));
+        return $item->getAttributes();
     }
 
 }
---
+++ www/api/index.php
@@ -0,0 +1,34 @@
+<?php
+ini_set('html_errors','off');
+require_once '../../vendor/autoload.php';
+$config = require_once '../../src/config.php';
+
+session_start();
+
+/* ************************ CONFIGURES DATABASE ACCESS ************************ */
+$db = $config['database'];
+\LaravelArdent\Ardent\Ardent::configureAsExternal([
+    'driver'    => 'pgsql',
+    'host'      => $db['host'],
+    'port'      => isset($db['port'])? $db['port'] : null,
+    'database'  => $db['name'],
+    'username'  => $db['user'],
+    'password'  => $db['pass'],
+    'charset'   => 'utf8',
+//    'collation' => 'utf8_unicode_ci'
+], 'en');
+
+
+/* ************************ CONFIGURES THE API OBJECTS ************************ */
+$r = new Luracast\Restler\Restler(PROD);
+
+$skip = ['.', '..'];
+foreach (scandir('../../src/API') as $file) {
+    if (!in_array($file, $skip)) {
+        $name = strtok($file, '.');
+        $r->addAPIClass("\\Shop\\API\\$name");
+    }
+}
+
+$r->addAPIClass(\Luracast\Restler\Explorer::class);
+$r->handle();
