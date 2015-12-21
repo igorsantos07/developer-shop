b5dae9b639ee7d0c21561136d701c161781172ba
Igor Santos <igorsantos07@gmail.com>
2015-12-21 03:00:14 -0200

    test: Verifying order after items were added

--- tests/api/CartCept.php
+++ tests/api/CartCept.php
@@ -9,7 +9,6 @@ $gen_item = function() use ($devs) {
 
 $I = new ApiTester($scenario);
 $I->wantTo('add items to cart');
-
 $item = $gen_item();
 $I->sendPUT('cart', $item);
 $I->seeCodeAndJson(201, $item);
@@ -17,7 +16,14 @@ $order = json_decode($I->grabResponse())->order_id;
 
 $I->amGoingTo('add another item to the same order');
 $new_item = $gen_item();
-$I->sendPUT('cart', $item);
-$I->seeCodeAndJson(201, $item);
+$I->sendPUT('cart', $new_item);
+$I->seeCodeAndJson(201, $new_item);
 $order2 = json_decode($I->grabResponse())->order_id;
 $I->assertEquals($order, $order2, 'The two items are in the same order');
+
+$I->amGoingTo('Verify order');
+$I->sendGET('cart');
+$I->expectTo('see items in order');
+$I->seeCodeAndJson(200, ['items' => [$item, $new_item]]);
+$I->expectTo('see correct total');
+$I->seeResponseContainsJson(['total' => $item['price'] + $new_item['price']]);
